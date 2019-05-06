d0420571e915dfb45cb2632203e45a8e8538a67b
Shyam Jos <shyamjosepp@gmail.com>
2019-05-06 12:19:46 +0530

    added hostname and system load in response

--- flask_load_gen.py
+++ flask_load_gen.py
@@ -9,9 +9,10 @@ def hello():
 
 @app.route('/load', methods=['GET', 'POST'])
 def gen_load():
-
+    get_hostname = subprocess.check_output(['bash','-c', 'echo "Hostname:" ;hostname; echo " </br> System Load:"; uptime | cut -d ":" -f 5'])
     load_job = subprocess.check_output(['bash','-c', load_shell_cmd])
-    return("\nok! Process Started!\n")
+    response_msg="\nok! Process Started!\n </br>" + str(get_hostname, 'utf-8')
+    return (response_msg)
 
 if __name__ == '__main__':
      app.run(host='0.0.0.0', port=5000, debug=True)
